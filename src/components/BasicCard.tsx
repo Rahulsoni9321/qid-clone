c95b3edfa064bff42027b4004999b9fb6649cdb4
Rahulsoni3108 <90192605+Rahulsoni3108@users.noreply.github.com>
2024-04-26 19:04:50 +0530

    responsiveness

--- src/components/BasicCard.tsx
+++ src/components/BasicCard.tsx
@@ -2,9 +2,9 @@
 const BasicCard = ({classname,number,label,description}:{classname:string,number:string,label:string,description:string}) => {
   return (
     <div className={classname}>
-       <div className='font-semibold text-5xl font-serif text-white/20'>{number}</div>
-       <div className="text-4xl font-bold font-abc text-gray-100">{label}</div>
-       <div className="text-md font-abc  font-normal leading-tight text-[#7a7773]">{description}</div>
+       <div className='hidden md:block font-semibold text-5xl font-serif text-white/20'>{number}</div>
+       <div className=" text-xl md:text-4xl font-bold font-abc text-gray-100">{label}</div>
+       <div className="hidden md:block text-md font-abc  font-normal leading-tight text-[#7a7773]">{description}</div>
     </div>
   )
 }
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -36,12 +36,12 @@ const LandingPage = () => {
             src="\fsgdh43_2-e1684067541108.png"
           ></img>
         </div>
-        <div className="flex justify-between px-10 gap-8">
-          <div className="font-abc font-bold text-6xl text-gray-200">Two Base elements of Identity ecosystem</div>
-          <div className="flex justify-end gap-12 10/12 ">
+        <div className="flex md:flex-row flex-col justify-between  md:px-10 gap-8">
+          <div className="font-abc font-bold text-3xl md:text-6xl text-gray-200 md:max-w-md">Two Base elements of Identity ecosystem</div>
+          <div className="flex order-first md:-order-first  justify-center text-center md:justify-end gap-12 md:gap-12 w-full  ">
 
-          <BasicCard label={cardcontent[0].label} number={cardcontent[0].number} description={cardcontent[0].description} classname='rounded-[28px] flex flex-col gap-5 p-4 shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px] py-6 -mt-16 h-fit w-2/5 border-[1px] border-[#3a4239] bg-[#121e13]'></BasicCard>
-          <BasicCard label={cardcontent[1].label} number={cardcontent[1].number} description={cardcontent[1].description}  classname='rounded-[28px] flex flex-col gap-6 p-4 pb-7 -mt-8 h-fit w-2/5 border-[1px] border-[#3a4239] bg-[#1a1407]/60  '></BasicCard>
+          <BasicCard label={cardcontent[0].label} number={cardcontent[0].number} description={cardcontent[0].description} classname='md:rounded-[28px] rounded-[18px] flex flex-col  md:text-start text-center gap-5 p-4  md:py-6 md:-mt-16 -mt-10 h-fit w-1/2 md:w-3/5   border-[1px] border-[#3a4239] bg-[#121e13]'></BasicCard>
+          <BasicCard label={cardcontent[1].label} number={cardcontent[1].number} description={cardcontent[1].description}  classname='md:rounded-[28px] rounded-[18px] flex flex-col md:text-start text-center gap-6 p-4 h-fit md:pb-7 md:py-6 md:-mt-8 -mt-6  w-1/2 md:w-3/5 border-[1px] border-[#3a4239] bg-[#1a1407]/60  '></BasicCard>
           </div>
         </div>
       </MaxWidthWrapper>
@@ -63,7 +63,7 @@ const LandingPage = () => {
 
 
 const cardcontent=[{
-  label:"Managing",
+  label:"Manage",
   number:"1",
   description:"Consolidate all your IDs securely in one place with qid, making them easily accessible whenever you need them"
 },{
